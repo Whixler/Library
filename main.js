d4566498dfcc15bdbeb49b2af9db7466b1956ef9
Whixler <obdeijn.lotte@gmail.com>
2025-06-12 13:03:25 +0200

    created function to add books to array

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(title, author, pages, isRead){
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = read;
+const title = document.getElementById("titleField");
+const author = document.getElementById("authorField");
+const pages = document.getElementById("pagesField");
+const isRead = document.getElementById("readField");
+
+const newBookBtn = document.querySelector("#newBook");
+const table = document.querySelector(".table");
+const form = document.querySelector("#form");
+const addBook = document.querySelector(".addBook")
+
+newBookBtn.addEventListener("click",()=> form.classList.toggle("hide"));
+addBook.addEventListener("click", function(e){
+  submitForm();
+  e.preventDefault();
+});
+
+addBookToLibrary("Harry Potter", "J K Rowling", 340, true);
+addBookToLibrary("Lord of the rings", "J R Tolken", 900, false);
+
+function Book(title, author, pages, isRead) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-    const newBook = new Book(title, author, pages, isRead);
+function submitForm(){
+  addBookToLibrary(titleField.value, authorField.value, Number(pagesField.value), readField.checked);
+  form.reset();
+  form.classList.toggle("hide");
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    let newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
+    console.log({myLibrary})
 }
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+console.log(myLibrary);
